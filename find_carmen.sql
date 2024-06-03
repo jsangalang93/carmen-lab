19f4dbe9b5474c50440ea046e84c310732ebd8e2
jsangalang93 <jsangalang93@gmail.com>
2024-06-03 19:46:11 -0400

    final commit

--- find_carmen.sql
+++ find_carmen.sql
@@ -72,4 +72,4 @@ SELECT name FROM city WHERE id = 211;
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
-
+SELECT name FROM city WHERE population = 91084;
