cab90ceb1a4cb4978ac303eccb62a4e526766327
jsangalang93 <jsangalang93@gmail.com>
2024-06-04 17:40:48 -0400

    added NFL Assignment

--- find_carmen.sql
+++ find_carmen.sql
@@ -73,3 +73,68 @@ SELECT name FROM city WHERE id = 211;
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
 SELECT name FROM city WHERE population = 91084;
+
+--NFL REPO:
+
+-- 1. List the names of all NFL teams'
+ 
+ SELECT name FROM teams;
+
+-- 2. List the stadium name and head coach of all NFC teams
+  
+SELECT head_coach, stadium FROM teams WHERE conference = 'NFC',
+
+-- 3. List the head coaches of the AFC South
+  
+SELECT head_coach FROM teams WHERE division = 'South' AND conference = 'AFC';
+
+-- 4. The total number of players in the NFL
+
+SELECT COUNT(name) FROM players;
+
+-- 5. The team names and head coaches of the NFC North and AFC East
+
+SELECT name, head_coach FROM teams WHERE conference = 'NFC' AND division = 'North' OR conference = 'AFC' AND division = 'East';
+
+
+-- 6. The 50 players with the highest salaries
+  
+SELECT name FROM players ORDER BY salary DESC LIMIT 50;
+
+-- 7. The average salary of all NFL players
+ 
+SELECT AVG(salary) FROM players;
+
+-- 8. The names and positions of players with a salary above 10_000_000
+  
+SELECT name, position FROM players WHERE salary <= 10_000_000;
+
+-- 9. The player with the highest salary in the NFL
+
+SELECT name FROM players ORDER BY salary DESC LIMIT 1;
+ 
+-- 10. The name and position of the first 100 players with the lowest salaries
+  
+SELECT name FROM players ORDER BY salary ASC Limit 100;
+
+-- 11. The average salary for a DE in the nfl
+
+SELECT AVG(salary) FROM players WHERE position = 'DE';
+
+-- 12. The names of all the players on the Buffalo Bills
+
+SELECT id FROM teams WHERE name = 'Buffalo Bills';
+SELECT name FROM players WHERE team_id = 1;
+
+-- 13. The total salary of all players on the New York Giants
+  
+SELECT id FROM teams WHERE name = 'New York Giants';
+SELECT SUM(salary) FROM players WHERE team_id = 18;
+
+-- 14. The player with the lowest salary on the Green Bay Packers
+
+SELECT id FROM teams WHERE name = 'Green Bay Packers';
+SELECT name FROM players WHERE team_id = 23 ORDER BY salary ASC LIMIT 1;
+
+ 
+
